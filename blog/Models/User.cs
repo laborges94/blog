d3fc8aeff520acead937a24d057709f172f19dc7
lucas <lucas@FAIDELL6032>
2025-11-24 09:46:12 -0300

    Adicionar arquivos de projeto.

---
+++ blog/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace blog.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Bio { get; set; }
+        public string Image { get; set; }
+        public string Slug { get; set; }
+    }
+}
---
+++ blog/Program.cs
@@ -0,0 +1,26 @@
+﻿using blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+
+internal class Program
+{
+    private const string ConnectionString =
+    "Server=(localdb)\\MSSQLLocalDB;Database=Blog;Integrated Security=true;";
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        ReadUsers();
+    }
+
+    public static void ReadUsers()
+    {
+        using var connection = new SqlConnection(ConnectionString);
+
+        var user = connection.GetAll<User>();
+
+        foreach (var u in user)
+            Console.WriteLine($"{u.Id} - {u.Name} - {u.Email}");
+    }
+}
+
